425e678f3f97b736234bdd27e8e105dc0a40eb97
JohnsonBai <18519797228@163.com>
2024-10-29 21:37:36 +0800

    Modify a little ：）

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gobildapinpoint/GoBildaPinpointDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gobildapinpoint/GoBildaPinpointDriver.java
@@ -24,6 +24,7 @@ package org.firstinspires.ftc.teamcode.gobildapinpoint;
 
 import static com.qualcomm.robotcore.util.TypeConversion.byteArrayToInt;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.hardware.lynx.LynxI2cDeviceSynch;
 import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynchDevice;
@@ -493,7 +494,7 @@ public class GoBildaPinpointDriver extends I2cDeviceSynchDevice<I2cDeviceSynchSi
     /**
      * @return a Pose2D containing the estimated position of the robot
      */
-    public Pose2D getPosition(){
+    public Pose2d getPosition(){
         return new Pose2D(DistanceUnit.MM,
                 xPosition,
                 yPosition,
