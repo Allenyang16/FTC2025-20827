3f7662263666f4b99299ef8749bf7763f509901c
JohnsonBai <18519797228@163.com>
2024-10-29 20:10:37 +0800

    Control claw + Test claw

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testings/ServoTest.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.testings;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@TeleOp(name = "servo test")
+@Config
+public class ServoTest extends LinearOpMode {
+
+    private final Telemetry telemetry_M = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+    public static boolean read_only = false;
+    public static boolean reverse = false;
+    public static double servo_pos1 = 0.5;
+
+    public static String servo_name1 = "wrist";
+    private Servo servo0=null;
+
+    @Override
+    public void runOpMode() {
+
+        servo0 = hardwareMap.get(Servo.class, servo_name1);
+        if (reverse){
+            servo0.setDirection(Servo.Direction.REVERSE);
+        }
+        waitForStart();
+        while (opModeIsActive()) {
+            if (!read_only) {
+                servo0.setPosition(servo_pos1);
+//                servo1.setPosition(servo_pos2);
+                telemetry_M.addData("leftFront", servo0.getPosition());
+//                telemetry_M.addData("rightfront", servo1.getPosition());
+                telemetry_M.update();
+            }
+        }
+    }
+}
