8c977cbd5497a35a45889326306743c28b5e8140
W8sper <3186602172@qq.com>
2025-02-09 14:10:02 +0800

    Create PipelineTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testings/PipelineTest.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.testings;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+@TeleOp(name = "Pipeline Test", group = "Testing")
+public class PipelineTest extends LinearOpMode {
+    OpenCvCamera camera;
+
+    @Override
+    public void runOpMode() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        
+        camera.setPipeline(new SimplePipeline());
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("Camera Error", errorCode);
+                telemetry.update();
+            }
+        });
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+        }
+
+        camera.stopStreaming();
+    }
+
+    private static class SimplePipeline extends OpenCvPipeline {
+        @Override
+        public Mat processFrame(Mat input) {
+            return input;
+        }
+    }
+}
