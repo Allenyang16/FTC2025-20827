75d4d9fb55181d4c36c8ff992f7b470d78479d42
Mike Chen <102953329@qq.com>
2025-03-13 17:11:20 +0800

    update values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/uppersystems/SuperStructure.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/uppersystems/SuperStructure.java
@@ -88,30 +88,30 @@ public class SuperStructure {
 
 
     // WRIST
-    public static double WRIST_INTAKE = 0.49, WRIST_INTAKE_PARALLEL_GROUND = 0.73;
-    public static double WRIST_INTAKE_SPECIMEN = 0.8, WRIST_INTAKE_SPECIMEN_GROUND = 0.57;
+    public static double WRIST_INTAKE = 0.17, WRIST_INTAKE_PARALLEL_GROUND = 0.75;
+    public static double WRIST_INTAKE_SPECIMEN = 0.9, WRIST_INTAKE_SPECIMEN_GROUND = 0.4;
 
     // TODO: Retest
     public static double WRIST_RELEASE_BOX_HIGH = 0.85, WRIST_RELEASE_BOX_LOW = 0.28;
-    public static double WRIST_RELEASE_CHAMBER_HIGH = 0.73, WRIST_RELEASE_CHAMBER_LOW = 0.8;
-    public static double WRIST_RELEASE_CHAMBER_TELEOP = 0.9;
+    public static double WRIST_RELEASE_CHAMBER_HIGH = 0.75, WRIST_RELEASE_CHAMBER_LOW = 0.8;
+    public static double WRIST_RELEASE_CHAMBER_TELEOP = 0.75;
     // Spin Wrist
-    public static double SPINWRIST_INTAKE = 0.15;
+    public static double SPINWRIST_INTAKE = 0.12;
 
-    public static double SPINWRIST_INTAKE_0 = 0.15;
+    public static double SPINWRIST_INTAKE_0 = 0.12;
     public static double SPINWRIST_INTAKE_45 = 0.3;
-    public static double SPINWRIST_INTAKE_90 = 0.45;
+    public static double SPINWRIST_INTAKE_90 = 0.48;
 
     // TODO: CHANGE THE VALUE
-    public static double SPINWRIST_INTAKE_SPECIMEN = 0.77;
-    public static double SPINWRIST_RELEASE_SPECIMEN = 0.15;
-    public static double SPINWRIST_AUTO = 0.62;
+    public static double SPINWRIST_INTAKE_SPECIMEN = 0.86;
+    public static double SPINWRIST_RELEASE_SPECIMEN = 0.12;
+    public static double SPINWRIST_AUTO = 0.77;
     
     // Claw
     // TODO: TEST Value
-    public static double CLAW_OPEN = 0.52;
-    public static double CLAW_OPENLarge = 0.85;
-    public static double CLAW_GRAB = 0.28;
+    public static double CLAW_OPEN = 0.61;
+    public static double CLAW_OPENLarge = 0.61;
+    public static double CLAW_GRAB = 0.225;
     public ClawState clawState = GRAB;
     public SlideState slideState = SlideState.VERTICAL;
     public WristIntakeState wristIntakeState = WristIntakeState.PRE_INTAKE;
